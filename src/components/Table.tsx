dbec8f27121e00591ade4cd34f02d83d5ef67455
espezaliate <mkoziol54@gmail.com>
2022-02-14 14:59:11 +0100

    added styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Response, Data, Get } from "../Interfaces/table";
+import { Response, Data } from "../Interfaces/table";
 
 export const Table: React.FC = () => {
   const [search, setSearch] = useState("");
@@ -13,13 +13,39 @@ export const Table: React.FC = () => {
       .then((response): Promise<Response> => response.json())
       .then((parsedData) => {
         setData(parsedData.response.data);
-        console.log(data);
       });
-  });
+  }, []);
+
+  console.log(data);
+
+  const rows: [string][] = [
+    ["WO ID"],
+    ["Description"],
+    ["Received date"],
+    ["Status"],
+    ["Priority"],
+  ];
+
   return (
-    <div>
-      <div>
-        <input type="search" onInput={handleSearch}></input>
+    <div className="container">
+      <div className="center">
+        <input
+          type="search"
+          onInput={handleSearch}
+          placeholder="Search..."
+        ></input>
+      </div>
+      <div className="center">
+        <table>
+          <thead>
+            {rows.map((e) => (
+              <tr>
+                <th>{e}</th>
+              </tr>
+            ))}
+          </thead>
+          <tbody></tbody>
+        </table>
       </div>
     </div>
   );
